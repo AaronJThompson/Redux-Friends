0537979ff7cece9b586c6ca6c0e327fddbf7a314
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-20 14:27:41 +0100

    Add action types

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,1 +1,13 @@
 import { combineReducers } from 'redux';
+
+const initialState = {
+    deletingFriend: false,
+    fetchingFriends: false,
+    friends: [],
+    loggingIn: false,
+    addingFriend: false,
+    updatingFriend: false,
+    error: null
+}
+
+export function friendsReducer
